9231e58c2c733840f88fa266ef1621c70a2c63da
Amael Maserati <amael.maserati@etu.univ-orleans.fr>
2023-11-20 17:22:40 +0100

    Ajout de fonctions de base qui passent les tests pour un fichier nommé BilletBD

---
+++ Code/BD/BilletBD.py
@@ -0,0 +1,39 @@
+from BD import Spectateur, Festival, Type_Billet, Billet
+from ConnexionBD import ConnexionBD
+from sqlalchemy.sql.expression import text
+from sqlalchemy.exc import SQLAlchemyError
+
+class BilletBD:
+    def __init__(self, conx: ConnexionBD):
+        self.connexion = conx
+        
+    def get_billets_spectateur(self, festival, type_billet, spectateur):
+        try:
+            query = text("SELECT idB, idF, idType, idS, prix, dateAchat FROM BILLET WHERE idF = :idFestival AND idType = :idType AND idS = :idSpectateur")
+            billets = []
+            result = self.connexion.get_connexion().execute(query, {"idFestival": festival.get_idF(), "idType": type_billet.get_idType(), "idSpectateur": spectateur.get_idS()})
+            for idB, idF, idType, idS, prix, dateAchat in result:
+                billets.append(Billet(idB, festival, type_billet, spectateur, prix, dateAchat))
+            return billets
+        except SQLAlchemyError as e:
+            print(f"La requête a échoué : {e}")
+            
+    def insert_billet(self, billet):
+        try:
+            query = text("INSERT INTO billet (idF, idType, idS, prix, dateAchat) VALUES (:idF, :idType, :idS, :prix, :dateAchat)")
+            result = self.connexion.get_connexion().execute(query, {"idF": billet.get_idFestival(), "idType": billet.get_idType(), "idS": billet.get_idSpectateur(), "prix": billet.get_prix(), "dateAchat": billet.get_dateAchat()})
+            billet_id = result.lastrowid
+            print(f"Le billet {billet_id} a été ajouté")
+            self.connexion.get_connexion().commit()
+        except SQLAlchemyError as e:
+            print(f"La requête a échoué : {e}")
+            
+    def delete_billet_by_id_spectateur(self, billet, id_spectateur):
+        try:
+            query = text("DELETE FROM billet WHERE idB = :idB AND idS = :idS")
+            self.connexion.get_connexion().execute(query, {"idB": billet.get_idB(), "idS": id_spectateur})
+            print(f"Le billet {billet.get_idB()} a été supprimé")
+            self.connexion.get_connexion().commit()
+        except SQLAlchemyError as e:
+            print(f"La requête a échoué : {e}")
+        
