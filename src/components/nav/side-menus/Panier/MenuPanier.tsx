bf759bf3b352fdcee6f2368e87bd9da4542dc665
Sébastien Gratade <sebastien.gratade@etu.univ-orleans.fr>
2023-11-25 18:16:01 +0100

    la croix des side menus est responsive

--- src/components/nav/side-menus/Panier/MenuPanier.tsx
+++ src/components/nav/side-menus/Panier/MenuPanier.tsx
@@ -36,7 +36,10 @@ export default function MenuConnexion(props: Props) {
     initial="hidden"
     animate={props.isOpen ? "visible" : "hidden"}>
         <div className="cross" onClick={() => props.setIsOpen(false)}>
-          <img src="/icones/cross.svg" alt="croix" />
+            <svg width="36" height="28" viewBox="0 0 36 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect x="6.52539" y="0.321533" width="35.8974" height="3.58974" rx="1.79487" transform="rotate(45 6.52539 0.321533)" fill="#E45A3B"/>
+              <rect x="3.87891" y="25.5957" width="35.8974" height="3.58974" rx="1.79487" transform="rotate(-45 3.87891 25.5957)" fill="#E45A3B"/>
+            </svg>
         </div>
         <div className="container">
           <h2>Mon panier</h2>
