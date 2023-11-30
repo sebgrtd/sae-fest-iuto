ee33d11c1d137ed69b23b76e33b6ba17fb5e5488
seb_grtd <sebastien.gratade@etu.univ-orleans.fr>
2023-11-30 16:15:59 +0100

    ajout de messages d'erreur et d'animations lorsqu'il y a une erreur lors de l'envoi d'un formulaire du side menu de connexion. Ajout d'une animation de chargement sur les boutons lors de l'envoie des formulaires pour que le fait que la page charge soit plus clair

--- src/components/form/ChampCode.tsx
+++ src/components/form/ChampCode.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import { useEffect, useRef,useState } from 'react'
 
 type Props = {
     codeVar: string;
     setCodeVar: (codeVar: string) => void;
     nbChar: number;
+    errorText: string;
 }
 
 export default function ChampCode(props : Props) {
-  const champCodeRef = React.useRef<HTMLDivElement>(null)
+  const champCodeRef = useRef<HTMLDivElement>(null)
 
+  const firstCharRef = useRef<HTMLInputElement>(null)
+  const secondCharRef = useRef<HTMLInputElement>(null)
+  const thirdCharRef = useRef<HTMLInputElement>(null)
+    const fourthCharRef = useRef<HTMLInputElement>(null)
+    const fifthCharRef = useRef<HTMLInputElement>(null)
+    const sixthCharRef = useRef<HTMLInputElement>(null)
+ 
+  const[hasLoaded, setHasLoaded] = useState(false)
+     
   const setNextInFocus = (index: number) => {
     const nextInput = champCodeRef.current?.querySelectorAll("input")[index]
     if(nextInput){
@@ -16,26 +26,56 @@ export default function ChampCode(props : Props) {
     }
   }
 
+  useEffect(() => {
+    // change le texte dans les input
+    if (props.codeVar == "") {
+        firstCharRef.current?.setAttribute("value", "")
+        secondCharRef.current?.setAttribute("value", "")
+        thirdCharRef.current?.setAttribute("value", "")
+        fourthCharRef.current?.setAttribute("value", "")
+        fifthCharRef.current?.setAttribute("value", "")
+        sixthCharRef.current?.setAttribute("value", "")
+    }
+  }, [props.codeVar])
+
+  useEffect(() => {
+    // mets le premier input en focus
+    if (!hasLoaded) {
+        firstCharRef.current?.focus()
+        setHasLoaded(true)
+    }
+  }, [sixthCharRef.current])
+  
+
   return (
-    <div className="champ-code" ref={champCodeRef}>
+    <div className="container-champ-code">
+        <div className="champ-code" ref={champCodeRef}>
+
+            {
+                Array.from(Array(props.nbChar).keys()).map((index) => {
+                    return(
+                        <input 
+                            key={index}
+                            type="text" 
+                            ref={index == 0 ? firstCharRef : index == 1 ? secondCharRef : index == 2 ? thirdCharRef : index == 3 ? fourthCharRef : index == 4 ? fifthCharRef : sixthCharRef}
+                            maxLength={1} 
+                            value={props.codeVar[index]} 
+                            onChange={(e) => {
+                                const newCode = [...props.codeVar]
+                                newCode[index] = e.target.value
+                                props.setCodeVar(newCode.join(""))
+                                setNextInFocus(index+(e.target.value.length == 0 ? -1 : 1))
+                            }}
+                        />
+                    )
+                })
+            }
+
+        </div>
 
         {
-            Array.from(Array(props.nbChar).keys()).map((index) => {
-                return(
-                    <input 
-                        key={index}
-                        type="text" 
-                        maxLength={1} 
-                        value={props.codeVar[index]} 
-                        onChange={(e) => {
-                            const newCode = [...props.codeVar]
-                            newCode[index] = e.target.value
-                            props.setCodeVar(newCode.join(""))
-                            setNextInFocus(index+(e.target.value.length == 0 ? -1 : 1))
-                        }}
-                    />
-                )
-            })
+            props.errorText !== "" &&
+                <p className="error">{props.errorText}</p>
         }
 
     </div>
