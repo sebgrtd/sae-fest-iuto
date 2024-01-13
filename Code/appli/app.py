c0525788430bea627baabeaf3fab1f81a2e72ade
araviart <alexandre.raviart@etu.univ-orleans.fr>
2024-01-13 17:34:49 +0100

    fix connexion et inscription avec statutUser

--- Code/appli/app.py
+++ Code/appli/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_file
+import datetime
+from flask import Flask, send_file, session
 from flask_cors import CORS
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
@@ -101,6 +102,7 @@ def connecter():
     email = data["email"]
     password = data["password"]
     if userbd.exist_user(email, password):
+
         return userbd.user_to_json(userbd.get_user_by_email(email))
     else:
         return jsonify({"error": "Utilisateur non trouve"})
@@ -119,11 +121,15 @@ def inscription():
     else:
         res = userbd.add_user(pseudo, email, password)
         if res:
-            return userbd.user_to_json(userbd.get_user_by_email(email))
+            user = userbd.get_user_by_email(email)
+            if user is not None:
+                return userbd.user_to_json(user)
+            else:
+                return jsonify({"error": "Utilisateur ajouté mais non retrouvé"})  # Nouvelle gestion d’erreur
         elif res == "emailErr":
-            return jsonify({"error": "Email déjà existant"})
+                return jsonify({"error": "Email déjà existant"})
         else:
-            return jsonify({"error": "Erreur lors de l'ajout de l'utilisateur"})
+                return jsonify({"error": "Erreur lors de l’ajout de l’utilisateur"})
         
 @app.route('/modifierProfil', methods=['POST'])
 def modifierProfil():
@@ -606,6 +612,8 @@ def types_billet_festival():
         return render_template("admin_types_billet.html", liste_types_billet=[])
     return render_template("admin_types_billet.html", liste_types_billet=liste_types_billet)
 
+
+
 @app.route("/spectateurs_festival")
 def spectateurs_festival():
     connexionbd = ConnexionBD()
@@ -681,6 +689,32 @@ def modifier_user():
         print(f"La mise à jour de l'utilisateur {id_user} a échoué.")
     return redirect(url_for("users_festival"))
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+@app.route('/reserver_billets', methods=['POST'])
+def reserver_billets():
+    connexion_bd = ConnexionBD()
+    userbd = UserBD(connexion_bd)
+    data = request.get_json()
+    idUser =data['id']
+    data = request.get_json()
+    billets_a_ajouter = []
     
+    for billet_json in data['billets']:
+        date_debut_b = datetime.strptime(billet_json['selectedDaysSortedString'].split('-')[0], '%d %b %Y').date() if billet_json['selectedDaysSortedString'] else None
+        date_fin_b = datetime.strptime(billet_json['selectedDaysSortedString'].split('-')[1], '%d %b %Y').date() if billet_json['selectedDaysSortedString'] else None
+        id_festival = 1 
+        
+        # Exemple d'ajout des billets à la base de données
+        for i in range(billet_json['quantity']):
+            billet = Billet(
+                idF=id_festival,
+                idType=billet_json['id'],
+                idS=idUser,
+                prix=billet_json['price'],
+                dateAchat=datetime.utcnow(),
+                dateDebutB=date_debut_b,
+                dateFinB=date_fin_b
+            )
+            billets_a_ajouter.append(billet)
+    
+    db.session.add_all(billets_a_ajouter)
+    db.session.commit()
