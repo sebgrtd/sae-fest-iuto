2ad00a6acae4b3dfbbf78af1c2d07289e6c01a20
Sébastien Gratade <sebastien.gratade@etu.univ-orleans.fr>
2024-01-19 22:40:19 +0100

    fix du responsive de la page billeterie

--- src/pages/Billeterie/Billeterie.tsx
+++ src/pages/Billeterie/Billeterie.tsx
@@ -62,10 +62,12 @@ export default function Billeterie(props: Props) {
     <>
       <div className="page-defaut" id="Billeterie">
         <header>
-        <video className="bgbillet" autoPlay muted loop>
-            <source src={videoSrc} type="video/mp4" />
-            Votre navigateur ne supporte pas la vidéo.
-          </video>
+            <div className="container-video">
+              <video className="bgbillet" autoPlay muted loop>
+                <source src={videoSrc} type="video/mp4" />
+                Votre navigateur ne supporte pas la vidéo.
+              </video>
+            </div>
             <h2>BILLETERIE</h2>
                 <img
                   src="images/billet_pass1j.png"
@@ -102,6 +104,7 @@ export default function Billeterie(props: Props) {
                 viewBox="0 0 64 64"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
+                className="etoile"
               >
                 <path
                   d="M62.9991 27.739L42.1815 27.7675L56.8787 13.0286L50.7001 6.86056L36.0029 21.5994L35.9744 0.785744L27.2406 0.797718L27.2692 21.6114L12.5316 6.91288L6.36413 13.0979L21.1017 27.7964L0.289932 27.825L0.301899 36.5537L21.1137 36.5251L6.41646 51.2641L12.6009 57.4321L27.2981 42.6932L27.3266 63.5069L36.0603 63.4949L36.0318 42.6812L50.7694 57.3798L56.931 51.1948L42.1934 36.4962L63.011 36.4677L62.9991 27.739Z"
