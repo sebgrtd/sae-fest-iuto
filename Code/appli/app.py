1f26096c920ccf3b5664e0b27b7f6d8e5388870e
Sébastien Gratade <sebastien.gratade@etu.univ-orleans.fr>
2024-01-21 20:07:28 +0100

    début génération de pdfs

--- Code/appli/app.py
+++ Code/appli/app.py
@@ -1039,3 +1039,160 @@ def getMesBillets():
     connexion_bd.fermer_connexion()
     print(billets, idUser)
     return billets
+
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.colors import HexColor
+from reportlab.platypus import Table, TableStyle
+from reportlab.lib.colors import Color
+
+
+transparent = Color(0, 0, 0, alpha=0)
+
+
+pdfmetrics.registerFont(TTFont('TuskerGrotesk', './gen_pdf/tusker-grotesk-font/TuskerGrotesk-3700Bold.ttf'))
+pdfmetrics.registerFont(TTFont('Bold', './gen_pdf/montserrat/Montserrat-Bold.ttf'))
+pdfmetrics.registerFont(TTFont('Regular', './gen_pdf/montserrat/Montserrat-Regular.ttf'))
+color_orange = HexColor("#E45A3B")
+color_orange_secondary= HexColor("#E97B62")
+color_dark = HexColor("#757A80")
+color_darker = HexColor("#19212C")
+color_white = HexColor("#FFFFFF")
+
+@app.route('/telecharger_billet', methods=['POST'])
+def telecharger_billet():
+    connexion_bd = ConnexionBD()
+    billetbd = BilletBD(connexion_bd)
+    """
+    axios.post('http://localhost:8080/telecharger_billet', {
+        responseType: 'blob',    
+        data: props.billet
+        })
+    """
+    data = request.get_json()
+    print(data)
+    idBillet = data["data"]["idB"]
+    print(idBillet)
+    billet = billetbd.get_billet_by_id(idBillet)
+    print(billet.to_dict())
+    
+    
+    img_path = "./gen_pdf/img"
+    logo_path = img_path + "/logo.jpg"
+    img_billet_path = img_path + "/billet.jpg"
+    width, height = A4
+    
+    c = canvas.Canvas("billet.pdf")
+    
+    # ajoute l'image tout en haut
+    c.drawImage(logo_path, 59, height-100, width=167, height=74)
+ 
+    styles = getSampleStyleSheet()
+    style = styles["BodyText"]
+    style.fontSize = 14
+    style.textColor = color_orange
+    style.alignment = 2
+    
+    p = Paragraph("FEST IUT’O", style=styles["BodyText"])
+    p.wrapOn(c, 200, 600)
+    p.drawOn(c, width-250, height-45)
+    
+    p = Paragraph("Olivet, 45160", style=styles["BodyText"])
+    p.wrapOn(c, 200, 600)
+    p.drawOn(c, width-250, height-65)
+    
+    p = Paragraph("21 au 23 juin 2024", style=styles["BodyText"])
+    p.wrapOn(c, 200, 600)
+    p.drawOn(c, width-250, height-84)
+    
+    
+    style.alignment = 0
+    style.fontName="Bold"
+    
+    p = Paragraph("Mes billets", style=styles["BodyText"])
+    p.wrapOn(c, 200, 600)
+    p.drawOn(c, 59, height-150)
+    
+    style.alignment = 2
+    style.textColor = color_orange_secondary
+    style.fontName="Regular"
+    p = Paragraph("GRATADE SEBASTIEN", style=styles["BodyText"])
+    p.wrapOn(c, 200, 600)
+    p.drawOn(c, width-250, height-150)
+    
+    # en dessous de ces deux textes je veux mettre le billet en grand (qui prenne toute la largeur)
+    
+    #c.drawImage(img_billet_path, 59, height-413, width=484, height=167.3)
+    
+    # je veux dessiner un tableau horaire artiste genre scene
+    
+    tableau = [["Horaire", "Artiste", "Genre", "Scene"]]
+    tableau.append(["14h00", "Muse", "Rock", "Scene 1"])
+    tableau.append(["15h00", "Muse", "Rock", "Scene 1"])
+    tableau.append(["16h00", "Muse", "Rock", "Scene 1"])
+    tableau.append(["16h00", "Muse", "Rock", "Scene 1"])
+    
+    t = Table(tableau)
+    colWidth = (width-100)/4
+    t._argW = [colWidth] * 4
+    
+    # Avant mon tableau j'ai envie d'ajouter un texte en darker qui dirait 22 JUILLET
+    
+    style.alignment = 0
+    style.fontSize = 14
+    style.textColor = color_darker
+    style.fontName="Bold"
+    p = Paragraph("22 JUILLET", style=styles["BodyText"])
+    p.wrapOn(c, 200, 600)
+    p.drawOn(c, 59, height-200)
+
+    # Ajoutez un style au tableau
+    t.setStyle(TableStyle([
+    ('BACKGROUND', (0, 0), (-1, 0), color_darker),
+    ('TEXTCOLOR', (0, 0), (-1, 0), color_white),  # Couleur du texte pour l'en-tête
+    ('TEXTCOLOR', (0, 1), (-1, -1), color_darker),  # Couleur du texte pour le reste du tableau
+
+    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+    ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+    ('FONTSIZE', (0, 0), (-1, 0), 14),
+
+    ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+    ('BACKGROUND', (0, 1), (-1, -1), transparent),
+    ]))
+
+    # Dessinez le tableau sur le canvas
+    t.wrapOn(c, 500, 600)
+    t.drawOn(c, 59, height-(63*len(tableau)))
+    
+    # en dessous je veux écrire les conditions du billet:
+    
+
+    text = u"""
+        1.Introduction au billet : "Félicitations ! Vous détenez maintenant votre billet pour l’édition 2024 de FEST IUT’O. Nous sommes ravis de vous accueillir à cet événement exceptionnel. Veuillez lire attentivement les informations ci-dessous pour vous assurer une expérience inoubliable."
+        <br/>2.Avis de sécurité : "Votre sécurité est notre priorité. Veuillez suivre les consignes de sécurité affichées sur le site du festival. En cas de besoin, n'hésitez pas à solliciter l'aide du personnel de sécurité. Les sacs seront soumis à une inspection à l'entrée pour assurer la sécurité de tous."
+<br/>3.Conditions d'admission : "Ce billet est votre pass d'entrée au festival. Assurez-vous de l'avoir sur vous en tout temps. L'accès au site sera refusé en l'absence de ce billet. Les billets sont non remboursables, veuillez les conserver en lieu sûr."
+<br/>4.Horaires et Artistes : "Consultez l'horaire officiel du festival pour connaître les horaires de chaque artiste et les performances spéciales. Planifiez votre journée en fonction de vos artistes préférés et des événements qui vous tiennent à cœur."
+<br/>5.Instructions pour l'entrée : "Pour faciliter votre entrée, veuillez arriver tôt. Présentez ce billet à l'entrée du festival, où il sera scanné pour vérification. Assurez-vous de suivre les indications du personnel pour un processus d'entrée fluide."
+<br/>6.Objets interdits : "Certaines articles sont interdits sur le site, y compris les armes, les substances illicites, les objets tranchants, etc. Consultez notre site web pour la liste complète des articles interdits. Tout non-respect de ces règles entraînera l'expulsion du festival."
+<br/>7.Engagement envers l'environnement : "Nous sommes fiers de notre engagement envers l'environnement. Aidez-nous à maintenir la propreté du site en utilisant les poubelles et en respectant l'environnement naturel qui nous entoure."
+<br/>8.Contact d'urgence : "En cas d'urgence, veuillez contacter le personnel de sécurité ou les points d'information sur le site. 
+<br/>9.Remerciements et encouragement : "Nous vous remercions d'être des participants essentiels à cette expérience. Profitez pleinement de FEST IUT’O et créez des souvenirs mémorables. Nous sommes ravis de partager ces moments exceptionnels avec vous."
+    """
+    
+    
+    style.alignment = 4
+    style.fontSize = 10
+    style.textColor = color_dark
+    
+    p = Paragraph(text, style=style)
+    p.wrapOn(c, 484, 600)
+    p.drawOn(c, 59, height-800)
+    
+    c.save()
+    
+    connexion_bd.fermer_connexion()
+    return send_file("billet.pdf", as_attachment=True)
